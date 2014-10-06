5825616701204ae9cba257e269a07bd95b925a81
Nishide Daisuke <d.nishide@gmail.com>
2014-10-06 14:42:25 +0800

    Commit Sample Tutorial Source Code.

--- TestTutorial/TestTutorialViewController.h
+++ TestTutorial/TestTutorialViewController.h
@@ -8,6 +8,9 @@
 
 #import <UIKit/UIKit.h>
 
-@interface TestTutorialViewController : UIViewController
+@interface TestTutorialViewController : UIViewController <UIScrollViewDelegate>
+
+@property(nonatomic,strong) UIScrollView *scrollView;
+@property(nonatomic,strong) UIPageControl *pageControl;
 
 @end
